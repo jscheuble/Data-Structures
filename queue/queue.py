1ebf7fa370d70b8dcb1e528ac0a9b58ff008f73c
Jana Scheuble <janascheuble@Janas-MacBook-Air.local>
2020-07-13 18:21:11 -0700

    queue with list

--- queue/queue.py
+++ queue/queue.py
@@ -13,16 +13,35 @@ return elements in First In First Out order.
 Stretch: What if you could only use instances of your Stack class to implement the Queue?
          What would that look like? How many Stacks would you need? Try it!
 """
+
+
 class Queue:
     def __init__(self):
         self.size = 0
-        # self.storage = ?
-    
+        self.storage = []
+
     def __len__(self):
-        pass
+        return self.size
 
     def enqueue(self, value):
-        pass
+        self.size = self.size + 1
+        self.storage.insert(0, value)
 
     def dequeue(self):
-        pass
+        if self.size > 0:
+            self.size = self.size - 1
+            return self.storage.pop()
+
+# class Queue:
+#     def __init__(self):
+#         self.size = 0
+#         self.storage = []
+
+#     def __len__(self):
+#         pass
+
+#     def enqueue(self, value):
+#         pass
+
+#     def dequeue(self):
+#         pass
